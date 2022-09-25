1579b673b8b5dfb2e99f86665c85fe1a07529be6
googlekeep <info.workingforyou@gmail.com>
2022-09-26 00:34:42 +0530

    verify email and password and check wheter valid user or not

---
+++ js/login.js
@@ -0,0 +1,26 @@
+document.getElementById('btn-sumit').addEventListener('click', function(){
+    // step-2: get the email address inside the input find
+    // always remeber to used .value to get text form  an input field
+    const emailField = document.getElementById('user-email',);
+    const email = emailField.value;
+    // step-3: get password
+    // 3.a: set id on the html element
+    // 3.b: get the element
+    // 3.c: get the value form the element
+
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+    
+    //  do not verify email password on the clinet side
+    // step-4: verify email and password and check wheter valid user or not
+
+    if(email === 'admin@gmail.com' && password ==='hello'){
+        console.log('valid user')
+    }
+    else{
+        console.log('invalid user')
+    }
+    
+})
+
