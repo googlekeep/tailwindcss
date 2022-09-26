1a22f1117f335f7a3210e63e1266881046017d8d
googlekeep <info.workingforyou@gmail.com>
2022-09-26 19:50:04 +0530

    diposit to balance total

--- js/deposit.js
+++ js/deposit.js
@@ -3,11 +3,31 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
     // step-2: get the deposit amount from the deposit input field
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
     //   step-3: get the current deposit total
     // for non-input(element other than input, textarea ) use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText =depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    // step-4: add numbers to set the total deposit
+    const currentDpositTotal = previousDepositTotal + newDepositAmount;
+    // set the deposit total
+    depositTotalElement.innerText =currentDpositTotal;
+
+    //  step-5 : get ballance total
+
+    const ballanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = ballanceTotalElement.innerText;
+    const previousBlanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step-6 : calculate current tptal blance
+    const currentBlanceTotal = previousBlanceTotal + newDepositAmount;
+    //  set the balance total
+    ballanceTotalElement.innerText = currentBlanceTotal;
+
+    // step-7 : clear the deposit field 
+    depositField.value = '';
 
 })
